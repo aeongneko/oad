07ed19a3ba953ae3cb1a542cc2ee08585e56fe59
aeongdesu <osu@kakao.com>
2023-09-11 00:17:30 +0900

    chore: remove beatconnect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const urlParams = new URLSearchParams(window.location.search)
 const consoleArea = document.getElementById("consoleArea")
 const basePath = window.location.href.split("?")[0]
@@ -8,7 +9,6 @@ const init = `- osu assets downloader
         * 0 or not specified: nerinyan (nerinyan.moe)
         * 1: mino (catboy.best)
         * 2: osu.direct
-        * 3: beatconnect (beatconnect.io)
     * example: ${basePath}?id=1061089
 
 - skips storyboard assets for optimization
@@ -18,7 +18,6 @@ const MAPDL_URL = [
     "https://proxy.nerinyan.moe/d/",
     "https://catboy.best/d/",
     "https://osu.direct/d/",
-    "https://beatconnect.io/d/"
 ]
 
 const getquery = (query) => {
@@ -34,49 +33,3 @@ const scrollWindow = () => window.scrollTo({
 const log = (content) => consoleArea.append(content + "\n")
 
 const start = () => {
-    if (window.location.search == "") return log(init)
-    const beatmapsetid = getquery("id")
-    const server = getquery("server")
-    if (isNaN(beatmapsetid)) return log("Invalid beatmap id\n\n" + init)
-    if (isNaN(server)) return log("Invalid server id\n\n" + init)
-
-    return download(beatmapsetid, server)
-}
-
-const download = async (id, server) => {
-    log(`downloading beatmapset on mirror id: ${server ?? 0}...`)
-
-    const serverURL = MAPDL_URL[server] ?? MAPDL_URL[0]
-
-    const blob = await fetch(serverURL + id).then(r => {
-        if (!r.ok) throw log("Error: " + r.status)
-        return r.blob()
-    })
-
-    log("Beatmap downloaded, extracting...")
-
-    const reader = new zip.ZipReader(new zip.BlobReader(blob))
-    const files = await reader.getEntries()
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i].filename
-        if (filename.startsWith("sb/")) continue
-
-        // wait, this is so bad
-        const ext = filename.split(".").pop()
-        if (ext == "mp3" || ext == "ogg" || ext == "wav") mime = `audio/${ext}`
-        else if (ext == "jpg" || ext == "png") mime = `image/${ext}`
-        else mime = "text/plain"
-
-        const fileBlob = await files[i].getData(new zip.BlobWriter(mime))
-        const a = document.createElement("a")
-        a.href = URL.createObjectURL(fileBlob)
-        a.appendChild(document.createTextNode(filename))
-        document.body.appendChild(a)
-        document.body.appendChild(document.createElement("br"))
-        scrollWindow()
-    }
-    await reader.close()
-    log("Extracted, done!")
-}
-
-start()
